5926c17e222cb4e0b045ca35fe6667a7fb751f18
oscar-me <oscar-me@outlook.com>
2023-05-28 21:44:19 -0600

    update

---
+++ modules/models.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+
+
+class LinearRegression(torch.nn.Module):
+    def __init__(self, in_features, out_features) -> None:
+        super(LinearRegression, self).__init__()
+        self.linear = torch.nn.Linear(in_features, out_features)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+
+class MLP(nn.Module):
+    def __init__(self, in_features, h_features, out_features) -> None:
+        super(MLP, self).__init__()
+        self.linear_stack = nn.Sequential(
+            nn.Linear(in_features, h_features),
+            nn.Linear(h_features, h_features),
+            nn.Linear(h_features, out_features)
+        )
+
+    def forward(self, x):
+        out = self.linear_stack(x)
+        return out
